876708ee0fadbf5fa62e82515b6d684661b3421b
amiljoseph03 <amiljoseph03@gmail.com>
2025-11-07 14:25:35 +0530

    components created

---
+++ src/Users/Pages/Allbook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Allbook() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Allbook
---
+++ src/Users/Pages/Viewbook.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Viewbook() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Viewbook
