484d0b1bef16bbc6505fbdb6389ea01396368dc1
amiljoseph03 <amiljoseph03@gmail.com>
2025-11-11 16:45:42 +0530

    adminheader

---
+++ src/Components/Adminheader.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Adminheader() {
+  return (
+    <div>
+      <div className="flex">
+        <div className="flex">
+            <img src="" alt="" />
+            <h1>bookstore</h1>
+        </div>
+
+        <button>
+            {' '}
+            Logout{' '}
+        </button>
+      </div>
+
+      <div className="w-full">
+        <marqueue>welcome,.....</marqueue>
+      </div>
+    </div>
+  )
+}
+
+export default Adminheader
